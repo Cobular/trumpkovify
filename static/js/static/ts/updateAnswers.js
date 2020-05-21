1d6bd040634e32baea43977f03dfa24eeb25a365
Jake <royalcows9@gmail.com>
2020-05-21 16:05:55 -0700

    Removed garbage

--- static/js/static/ts/updateAnswers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function doUpdateOnAnswerSubmit() {
-    $.getJSON("/newQuestion", function (d) {
-        $("text_1").text(d.text1);
-        $("text_2").text(d.text2);
-    });
-}
-addEventListener("answeredEvent", doUpdateOnAnswerSubmit);
-//# sourceMappingURL=updateAnswers.js.map
--- static/js/updateAnswers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function doUpdateOnAnswerSubmit() {
-    $.getJSON("/newQuestion", function (d) {
-        $("text_1").text(d.text1);
-        $("text_2").text(d.text2);
-    });
-}
-addEventListener("answeredEvent", doUpdateOnAnswerSubmit);
